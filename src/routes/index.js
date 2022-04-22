76e4572dd10d9e891374daddfd393f2fe8b05511
Hudson <hudson.santosr@gmail.com>
2022-04-22 16:59:35 -0300

    update responses

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const routes = express.Router()
 
+// Aplication Routes
 const usersRouter = require('./users')
+
+routes.get('/', function (req, res) {
+  res.status(200).send({
+    message: 'Hello world!',
+    endpoint: '/',
+  })
+})
 
 routes.use('/user', usersRouter)
 
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const usersRouter = express.Router()
 
 usersRouter.get('/', (req, res, next) => {
-  res.json({
+  res.status(200).send({
     message: 'Hello world!',
     endpoint: '/user',
   })
