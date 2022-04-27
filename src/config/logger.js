b987f659b3549de040e8096d6604d4a3571e326b
Hudson <hudson.santosr@gmail.com>
2022-04-26 23:48:08 -0300

    log files

--- src/config/logger.js
+++ src/config/logger.js
@@ -12,8 +12,8 @@ const logger = createLogger({
     logFormat
   ),
   transports: [
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
   ],
 })
 
